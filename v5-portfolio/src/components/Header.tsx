beb76cb2ac7c573e40b723843baeeddaedd58ee6
BlitzNoval <moodleyface12@gmail.com>
2026-08-12 00:57:06 +0200

    Remove home link on header name

--- v5-portfolio/src/components/Header.tsx
+++ v5-portfolio/src/components/Header.tsx
@@ -31,7 +31,7 @@ export function Header() {
     <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-[48%] lg:flex-col lg:justify-between lg:py-24">
       <div>
         <h1 className="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">
-          <a href="/">{site.name}</a>
+          {site.name}
         </h1>
         <h2 className="mt-3 text-lg font-medium tracking-tight text-slate-200 sm:text-xl">
           {site.title}
